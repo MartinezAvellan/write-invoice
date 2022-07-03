28b6d7f788d34e9cd1ba0c797032da835001ffe1
Hugo A Martinez <martinez.avellan@hotmail.com>
2022-07-03 17:08:11 +0100

    QT-4

---
+++ app/services/s3_service.py
@@ -0,0 +1,39 @@
+import boto3
+import botocore
+
+from app.utils.constants import REGION, WRITE_BUCKET
+
+
+class S3Service(object):
+    def __init__(self):
+        self.s3_resource = boto3.resource('s3', region_name=REGION)
+        self.s3_client = boto3.client('s3', region_name=REGION)
+
+    def get_file_to_write(self, request_id, body):
+        try:
+            original_file = self.s3_resource.Object(body['bucket_name'], body['file_name'])
+            original_file.load()
+        except botocore.exceptions.ClientError as e:
+            if e.response['Error']['Code'] == '404':
+                print('{}| file {} doesnt exist in a bucket {}'.format(request_id, body['file_name'], body['bucket_name']))
+            else:
+                print('{}| Error file {}  {}'.format(request_id, body['file_name'], str(e)))
+        else:
+            try:
+                write_file = self.s3_resource.Object(WRITE_BUCKET, body['file_name'])
+                write_file.load()
+            except botocore.exceptions.ClientError as e:
+                if e.response['Error']['Code'] == '404':
+                    print('{}| file {} doesnt exist in a bucket {}'.format(request_id, body['file_name'], WRITE_BUCKET))
+                    return original_file
+                else:
+                    print('{}| Error file {}  {}'.format(request_id, body['file_name'], str(e)))
+            else:
+                return write_file
+
+    def save_file(self, request_id, body, data):
+        try:
+            self.s3_client.put_object(Body=''.join(data), Bucket=WRITE_BUCKET, Key=body['file_name'])
+        except Exception as e:
+            print('{}| Error save file {}  {}'.format(request_id, body['file_name'], str(e)))
+            raise
--- app/utils/constants.py
+++ app/utils/constants.py
@@ -1,2 +1,3 @@
 REGION = 'sa-east-1'
 DATE_FORMAT = '%m/%d/%Y %H:%M:%S'
+WRITE_BUCKET = 'write-invoice'
